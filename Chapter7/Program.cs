edf579a4e1c81c199f12132bab389c56b7d086bf
Takashi Arai <31059@ojs.ac.jp>
2020-10-26 13:17:07 +0900

    3期　復習

--- Chapter7/Program.cs
+++ Chapter7/Program.cs
@@ -12,38 +12,57 @@ namespace Chapter7
     {
         static void Main(string[] args)
         {
+            // コンストラクタ呼び出し
+            var abbrs = new Abbreviations();
 
-            //7-1
-            var text = "Cozy lummox gives smart squid who asks for job pen";
-            Exercise1_1(text); //問題7.1.1
-        }
+            // Addメソッドの呼び出し例
+            abbrs.Add("IOC", "国際オリンピック委員会");
+            abbrs.Add("NPT", "核兵器不拡散条約");
 
-        static void Exercise1_1(string text)
-        {
-            //var dict = new Dictionary<char, int>();
-            var dict = new SortedDictionary<char, int>();
-            foreach (var ch in text)
+            //Countプロパティを呼び出して数を表示
+            Console.WriteLine(abbrs.Count);
+
+            //Removeメソッドを使用
+            if (abbrs.Remove("NPT"))
             {
-                char upc = char.ToUpper(ch);
-                if ('A'<= upc && upc <= 'Z')
-                {
-                    if (dict.ContainsKey(upc))
-                    {
-                        //登録済み
-                        dict[upc]++;
-                    }
-                    else
-                    {
-                        //未登録
-                        dict[upc] = 1;
-                    }
-                }
+                Console.WriteLine("削除成功");
             }
-            foreach (var item in dict)
+            else
             {
-                Console.WriteLine($"{item.Key}:{item.Value}");
+                Console.WriteLine("削除失敗");
             }
+
+            //7.2.3
             
+
+
+            // インデクサの利用例
+            var names = new[] { "WHO", "FIFA", "NPT", };
+            foreach (var name in names)
+            {
+                var fullname = abbrs[name];
+                if (fullname == null)
+                    Console.WriteLine("{0}は見つかりません", name);
+                else
+                    Console.WriteLine("{0}={1}", name, fullname);
+            }
+            Console.WriteLine();
+
+            // ToAbbreviationメソッドの利用例
+            var japanese = "東南アジア諸国連合";
+            var abbreviation = abbrs.ToAbbreviation(japanese);
+            if (abbreviation == null)
+                Console.WriteLine("{0} は見つかりません", japanese);
+            else
+                Console.WriteLine("「{0}」の略語は {1} です", japanese, abbreviation);
+            Console.WriteLine();
+
+            // FindAllメソッドの利用例
+            foreach (var item in abbrs.FindAll("国際"))
+            {
+                Console.WriteLine("{0}={1}", item.Key, item.Value);
+            }
+            Console.WriteLine();
         }
     }
 }
